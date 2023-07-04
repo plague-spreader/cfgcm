dcf5c889aa439441dd90456d5620aec4f537996d
plague-spreader <user@stocazzo2.localdomain>
2023-07-04 23:16:49 +0200

    Added a kind-of GUI

--- cfgcm.sh
+++ cfgcm.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 
 SUCCESS=0
 INVALID_ARGS=1
@@ -6,22 +6,27 @@ INVALID_SCHEME=2
 NO_PATH=3
 INVALID_PATH=4
 
+CFG_CM="${XDG_DATA_HOME:-$HOME/.local/share}/cfgcm/"
+
 function error_no_path() {
 	>&2 printf "Url does not have a path\n"
 	exit $NO_PATH
 }
 
-[ $# -lt 1 ] && {
-	>&2 printf "Usage: $0 cfgcm:<path>\n"
-	exit $INVALID_ARGS
+url_to_parse="$1"
+[ "$url_to_parse" ] || {
+	url_to_parse=$(find ${CFG_CM} -type f -iwholename '*/connect' |
+		sed -e 's:^.*/cfgcm/::g; s:/connect$::g' |
+		zenity --list --column="Where to connect") || exit $SUCCESS
+	url_to_parse="cfgcm:./$url_to_parse"
 }
 
-[[ "$1" =~ ^cfgcm:.* ]] || {
+[[ "$url_to_parse" =~ ^cfgcm:.* ]] || {
 	>&2 printf "Url must begin with \"cfgcm:\"\n"
 	exit $INVALID_SCHEME
 }
 
-url_to_parse="${1#cfgcm:}"
+url_to_parse="${url_to_parse#cfgcm:}"
 
 # discard authorities
 
